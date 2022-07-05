b1f681a5bfa2272972a5dce5852f5b1adf63ba67
Chani <lsc90726@gmail.com>
2022-07-06 00:37:28 +0900

    4.0 Auth & Login Form

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import AppRouter from "./Router";
+import React, { useState } from "react";
+import fbase, { authService } from "../fbase";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; Cwitter {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;
--- src/components/Router.js
+++ src/components/Router.js
@@ -3,8 +3,7 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
 
-const AppRouter = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
       <Routes>
