73fa42340c812ebfcfc704aeb27888fc53cb8fe6
Chani <lsc90726@gmail.com>
2022-07-07 00:56:07 +0900

    5.0 Creating Account & LogIn

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,20 @@
 import AppRouter from "./Router";
-import React, { useState } from "react";
-import fbase, { authService } from "../fbase";
+import React, { useEffect, useState } from "react";
+import { authService } from "../fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      //user의 유뮤를 파악하여 로그인 상태 변경
+      user ? setIsLoggedIn(true) : setIsLoggedIn(false);
+      setInit(true);
+    });
+  }, []);
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer>&copy; Cwitter {new Date().getFullYear()}</footer>
     </>
   );
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,24 +1,42 @@
 import React, { useState } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
-function Auth() {
+const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const onChange = (e) => {
+  const [newAccount, setNewAccount] = useState(true);
+  const auth = getAuth();
+  const [error, setError] = useState("");
+
+  const onChange = (event) => {
     const {
       target: { name, value },
-    } = e;
-    //비구조화할당
-    //e.target.name,e.target.value
-
+    } = event;
     if (name === "email") {
       setEmail(value);
     } else if (name === "password") {
       setPassword(value);
     }
   };
-  const onSubmit = (e) => {
-    e.preventDefault();
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        data = await createUserWithEmailAndPassword(auth, email, password);
+      } else {
+        data = await signInWithEmailAndPassword(auth, email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      setError(error.message);
+    }
   };
+  const toggleAccount = () => setNewAccount(!newAccount);
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -38,14 +56,20 @@ function Auth() {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value="Log In" />
+        <input
+          type="submit"
+          value={newAccount ? "Create Account" : "Sign In"}
+        />
+        {error}
       </form>
+      <span onClick={toggleAccount}>
+        {newAccount ? "Sign in" : "Create Account"}
+      </span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Github</button>
       </div>
     </div>
   );
-}
-
+};
 export default Auth;
